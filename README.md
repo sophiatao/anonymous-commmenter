123e07ecdb3b4d6ccb8637667d2cc1a628bb65ae
Tao Sophia <taos1@mcmaster.ca>
2018-09-19 15:51:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+## Anonymous classroom commenter
+Allows students to anonymously ask questions, and instructors to dynamically put up course information throughout lecture
+
+### Usage instructions for 2DM3 TAs
+1. Clone repository
+2. Update Theorems.txt with relevant theorems for the class
+3. `python3 app.py`
+4. Instruct students to connect to printed IP address and port
+5. Refresh page to see new comments or to fetch updated theorems
+
+### Troubleshooting
+1. Make sure everybody is connected to the same WiFi network
+2. Make sure incoming connections are allowed (check Firewall settings)
+
+### TODO
+1. Implement auto polling or webhooks so no refreshing is required
+2. Alert when students post new comment
+3. Alert when new theorems posted
+
+### Credits
+Base app cloned from https://github.com/macloo/flask-forms.git
---
+++ app.py
@@ -0,0 +1,60 @@
+# using python 3
+import socket
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+"""FILL IN INFORMATION HERE"""
+
+class_name = "2DM3 T04" #FILL IN YOUR TUTORIAL NUMBER
+ta_name = "Sophia Tao" #FILL IN YOUR NAME
+ta_email = "email@example.com" #FILL IN YOUR EMAIL
+
+""" YOU SHOULD NOT HAVE TO CHANGE ANYTHING BELOW THIS POINT"""
+
+info_header = "Theorems"
+comments = []
+info_file = "Resources/theorems.txt"
+
+def get_info_data():
+    with open(info_file) as f:
+        return f.read().splitlines()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'some?bamboozle#string-foobar'
+Bootstrap(app)
+app.config['BOOTSTRAP_SERVE_LOCAL'] = True
+
+class NameForm(FlaskForm):
+    comment = StringField('Comment', validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index.html', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    comment = form.comment.data
+    changed = False
+
+    if form.validate_on_submit():
+        comments.append(comment)
+        changed = True
+        form.comment.data = ""
+
+    return render_template('index.html',
+        form=form,
+        comments=comments,
+        info_data=get_info_data(),
+        class_name=class_name,
+        ta_name=ta_name,
+        ta_email=ta_email,
+        info_header=info_header,
+        )
+
+if __name__ == '__main__':
+    print("Make connections to: " + socket.gethostbyname(socket.gethostname()))
+    print("Port: 5000")
+    app.run(debug=True)
+    # app.run(debug=True, host='0.0.0.0')
